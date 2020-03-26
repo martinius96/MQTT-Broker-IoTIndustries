8db9afb07101c26a1f5babac7b71fa2a54362666
Martin <martinius96@gmail.com>
2020-03-26 15:43:12 +0100

    podpora Arduino + Ethernet W5100 / W5500

--- connect_PSK.ino
+++ connect_PSK.ino
@@ -1,16 +1,27 @@
-#ifdef ESP32
+#if defined(__AVR_ATmega168__) || defined(__AVR_ATmega328P__)
+#include <SPI.h>
+#include <Ethernet.h>
+//#include <Ethernet2.h> //pre Wiznet W5500 Ethernet shield
+#elif defined(ESP32)
 #include <WiFi.h>
 #elif defined(ESP8266)
 #include <ESP8266WiFi.h>
 #endif
 #include <PubSubClient.h>
 
-
+#if defined(ESP32) || defined(ESP8266)
+WiFiClient espClient;
 const char* ssid = "WIFI_NAME";
 const char* password = "WIFI_PASSWORD";
+#elif defined(__AVR_ATmega168__) || defined(__AVR_ATmega328P__)
+EthernetClient espClient;
+byte mac[] = { 0xAA, 0xBB, 0xCC, 0x81, 0x7B, 0x4A };
+//IPAddress ip(192, 168, 0, 2);
+//IPAddress dnServer(192, 168, 0, 1);
+//IPAddress gateway(192, 168, 0, 1);
+//IPAddress subnet(255, 255, 255, 0);
+#endif
 const char* mqtt_server = "mqtt.iotindustries.sk";
-
-WiFiClient espClient;
 PubSubClient client(espClient);
 unsigned long lastMsg = 0;
 char msg[50];
@@ -48,6 +59,8 @@ void reconnect() {
 }
 void setup() {
   Serial.begin(115200);
+  Serial.println("UART ready");
+#if defined(ESP32) || defined(ESP8266)
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -56,6 +69,17 @@ void setup() {
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+#elif defined(__AVR_ATmega168__) || defined(__AVR_ATmega328P__)
+  if (Ethernet.begin(mac) == 0) {
+    Ethernet.begin(mac);
+    //Ethernet.begin(mac, ip);
+    //Ethernet.begin(mac, ip, dns);
+    //Ethernet.begin(mac, ip, dns, gateway);
+    //Ethernet.begin(mac, ip, dns, gateway, subnet);
+  }
+  Serial.print("  DHCP assigned IP ");
+  Serial.println(Ethernet.localIP());
+#endif
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
 }
@@ -63,6 +87,7 @@ void loop() {
   if (!client.connected()) {
     reconnect();
   }
+#if defined(ESP32) || defined(ESP8266)
   if (WiFi.status() != WL_CONNECTED) {
     WiFi.begin(ssid, password);
   }
@@ -70,6 +95,15 @@ void loop() {
     delay(500);
     Serial.print(".");
   }
+#elif defined(Arduino)
+  if (Ethernet.begin(mac) == 0) {
+    Ethernet.begin(mac);
+    //Ethernet.begin(mac, ip);
+    //Ethernet.begin(mac, ip, dns);
+    //Ethernet.begin(mac, ip, dns, gateway);
+    //Ethernet.begin(mac, ip, dns, gateway, subnet);
+  }
+#endif
   client.loop();
   if (millis() - lastMsg > 10000) {
     char pocitadlo_pole[8];
